5353f3c5e0af4f9df36af57fa632dc8140b48813
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-28 14:51:37 -0300

    feature: estruturação do componente de produto

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import productsData from '../data/products_mocks.json'
 
 const Catalog = () => {
-  return (
-    <div>Catalog</div>
-  )
-}
 
+  return (
+    <div>
+        Catalog
+    </div>
+  )
+
+}
 export default Catalog
